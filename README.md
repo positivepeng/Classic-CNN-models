37952b05fac1bc2e88e89a127786d102ba273348
positivepeng <903871772@qq.com>
2019-07-25 17:52:15 +0800

    firt commit - list CNN architecures and finish LeNet

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Classical-DNNs
+# Classic-DNN-models
 使用keras实现经典深层神经网络
+
+- [x] LeNet
+- [ ] AlexNet
+- [ ] ZFNet
+- [ ] VGGNet
+- [ ] GoogLeNet
+- [ ] Resnet
+- [ ] DenseNet
---
+++ VGG/Vgg13.py
@@ -0,0 +1,39 @@
+from keras.models import Sequential
+from keras.layers import Dense,Flatten,Dropout
+from keras.layers.convolutional import Conv2D,MaxPooling2D
+import numpy as np
+np.random.seed(7)
+
+model = Sequential()
+# 注意padding = same时，意味着卷积核移动时只要左边边界不出界，右边就进行填充，所以输出的大小为输入大小/步长 的上取整
+# 而padding = valid时，意味着卷积核移动中整个卷积核都不能出界
+model.add(Conv2D(64,(3,3),strides=(1,1),input_shape=(224,224,3),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(128,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(Conv2D(128,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(4096,activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(4096,activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(1000,activation='softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])
+model.summary()
